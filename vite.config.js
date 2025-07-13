14563c23ebca690d3bd7b60103eaffdda10cfcca
Abeer Abdullah <abdullahabeer179@gmail.com>
2025-07-13 15:54:57 +0500

    Updated deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/Shoplite/'
-
+  plugins: [react(), tailwindcss()],
+  base: '/Shoplite/', // ✅ case-sensitive correct
 })
